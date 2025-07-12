19a720df9b6944533ec6ed5e8501fd8647a1f012
lily23445 <lily23445@example.com>
2025-07-12 20:11:35 +0530

    second commit 4

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,6 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+
+  // 👇 Add overrides here
+  {
+    rules: {
+      "react/no-unescaped-entities": "off", // disable unescaped entity rule
+    },
+  },
+];
 
 export default eslintConfig;
